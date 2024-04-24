a7e0c4345f1eecbfe433c1dd6e3aa08e9058c26d
unknown <rishabh.th25@gmail.com>
2024-04-24 15:45:39 +0530

    design changes: animation lines order, padding issue

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className="bg-[#E0FFFA] w-full px-0 py-[24px] md:px-[48px] md:py-[80px] flex flex-col items-center gap-[24px] scrollbar-hide">
       <Text></Text>
-      <div className="flex justify-center items-center md:h-[549.02px] md:w-[791.02px] w-[375px] h-[260.28px]">
+      <div className="flex justify-center items-center md:h-[549.02px] md:w-[791.02px] w-[375px] h-[260.28px] px-[16px] md:px-0">
         <Icon></Icon>
       </div>
     </div>
